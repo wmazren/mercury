5faf28fec0935849e6faf02f03745d5d4965a9a1
Azren <w.m.azren@gmail.com>
2012-06-18 19:56:24 +0800

    subdomain

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,9 @@ class Account < ActiveRecord::Base
   has_many :users, :inverse_of => :account, :dependent => :destroy
   
   validates :name, :subdomain, :presence => true, :uniqueness => true
+  validates_format_of :subdomain, with: /^[a-z0-9_]+$/, message: "must be lowercase alphanumerics only"
+  validates_length_of :subdomain, maximum: 32, message: "exceeds maximum of 32 characters"
+  validates_exclusion_of :subdomain, in: ['www', 'mail', 'ftp'], message: "is not available"
   
   accepts_nested_attributes_for :users
   
